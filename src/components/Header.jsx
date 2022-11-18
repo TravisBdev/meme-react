8f942836bef57c0bfc069fad2bf378aa9378e6da
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2022-11-18 13:22:38 -0500

    it's meme'n time

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <img
+        src="src\assets\trollface.png"
+        alt="troll face"
+        className="troll-face"
+      />
+      <h2 className="title">Meme Generator</h2>
+      <h4 className="course-name">React Course - Project 3</h4>
+    </header>
+  );
+}
