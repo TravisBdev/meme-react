43ea7dfd9230691edf61fb95b06e1cf1d1615580
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2022-11-21 15:37:41 -0500

    added a new Meme component - styled the component according to the specs in figma

---
+++ src/components/Meme.jsx
@@ -0,0 +1,11 @@
+export default function Meme() {
+  return (
+    <main>
+      <form action="" className="meme-form">
+        <input type="text" className="input" placeholder="Top Text" />
+        <input type="text" className="input" placeholder="Bottom Text" />
+        <button className="memeBtn">Get a new meme image 😂</button>
+      </form>
+    </main>
+  );
+}
