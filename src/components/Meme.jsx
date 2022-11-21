3c55175bce3b995ce1e6b677ed82068f5daad888
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2022-11-21 17:41:03 -0500

    added a handleClick function to recognize when the button is clicked and add a new image from the api

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,10 +1,16 @@
 export default function Meme() {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log("button was clicked");
+  }
   return (
     <main>
       <form action="" className="meme-form">
         <input type="text" className="input" placeholder="Top Text" />
         <input type="text" className="input" placeholder="Bottom Text" />
-        <button className="memeBtn">Get a new meme image 😂</button>
+        <button onClick={handleClick} className="memeBtn">
+          Get a new meme image 😂
+        </button>
       </form>
     </main>
   );
