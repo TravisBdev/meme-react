9151630c7e9778c7a2f28d116250458b186ae402
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2022-11-23 12:27:16 -0500

    refactored the jsx in the meme component to use a div instead of a form so that I did not have to prevent the default submission

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,17 +1,16 @@
 export default function Meme() {
-  function handleClick(e) {
-    e.preventDefault();
+  function handleClick() {
     console.log("button was clicked");
   }
   return (
     <main>
-      <form action="" className="meme-form">
+      <div action="" className="meme-form">
         <input type="text" className="input" placeholder="Top Text" />
         <input type="text" className="input" placeholder="Bottom Text" />
         <button onClick={handleClick} className="memeBtn">
           Get a new meme image 😂
         </button>
-      </form>
+      </div>
     </main>
   );
 }
